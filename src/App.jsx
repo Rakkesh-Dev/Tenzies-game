98f05836b522d140d6765cae40602f7b8063ab7d
Rakkesh-Dev <dmrakkeshofficial@gmail.com>
2026-07-15 10:58:13 +0530

    setup completed

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <main>
+      Main element goes here
+    </main>
+  )
+}
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-DOM/client"
+import App from "./App"
+
+ReactDOM
+  .createRoot(document.getElementById("root"))
+  .render(<App />)
