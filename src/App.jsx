14099f5641e544edd9590c94b8f2f1e73bdb1fbc
Rakkesh-Dev <dmrakkeshofficial@gmail.com>
2026-07-17 10:34:26 +0530

    change dice color to green when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Die from "./Die"
 import { nanoid } from "nanoid"
 
 export default function App() {
-    
     const [dice, setDice] = useState(generateAllNewDice())
-    
+
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
@@ -15,21 +14,34 @@ export default function App() {
                 id: nanoid()
             }))
     }
-    
+
     function rollDice() {
         setDice(generateAllNewDice())
     }
-    
-    const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />)
-    
+
+    function hold(id) {
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        ))
+    }
+
+    const diceElements = dice.map(dieObj => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
+        />
+    ))
+
     return (
         <main>
             <div className="dice-container">
                 {diceElements}
             </div>
-            
             <button className="roll-dice" onClick={rollDice}>Roll</button>
-            
         </main>
     )
 }
